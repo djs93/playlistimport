455c896176718a3d5620455c47805cb78d6de27e
Dale <djs93@njit.edu>
2022-02-15 18:57:27 -0500

    Create function to parse query input to list of queries

---
+++ CustomDataCleaners.cs
@@ -0,0 +1,23 @@
+﻿namespace playlistimport;
+
+public static class CustomDataCleaners
+{
+    public static List<int> CleanQueryInput(string? queryInput)
+    {
+        List<int> resultList = new List<int>();
+        if (queryInput == null)
+        {
+            return resultList;
+        }
+        
+        string cleanedQueryStr = queryInput.Replace(" ", "").Replace(",", "");
+        int maxQueryVal = (int)Enum.GetValues(typeof(CustomQueries.AvailableQueries)).Cast<CustomQueries.AvailableQueries>().Max();
+        resultList.AddRange(
+            from letter in cleanedQueryStr 
+            where char.IsDigit(letter) && int.Parse(letter.ToString()) <= maxQueryVal
+            select int.Parse(letter.ToString())
+        );
+
+        return resultList;
+    }
+}
