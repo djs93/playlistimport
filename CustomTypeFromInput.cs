d3fbe43a991352949532ecf80d376e5a8ac4f861
Dale <djs93@njit.edu>
2022-02-15 18:22:11 -0500

    Add custom user input query function

---
+++ CustomTypeFromInput.cs
@@ -0,0 +1,17 @@
+﻿namespace playlistimport;
+
+public class CustomTypeFromInput
+{
+    public static string GetQueries(string message, bool retry, string retryMessage = "")
+    {
+        var queryQuestion = $"{message}\n Available queries are:\n {CustomQueries.GetAvailableQueries()}";
+        Console.WriteLine(message);
+        var readVal = Console.ReadLine();
+        while (retry && string.IsNullOrEmpty(readVal))
+        {
+            Console.WriteLine(retryMessage);
+            readVal = Console.ReadLine();
+        }
+        return !string.IsNullOrEmpty(readVal) ? readVal : "none";
+    }
+}
