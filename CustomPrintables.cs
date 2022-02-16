e09bfcea5714e7e11c6510860271169dc531dcc2
Dale <djs93@njit.edu>
2022-02-15 19:23:34 -0500

    Catch null and empty lists before printing table

--- CustomPrintables.cs
+++ CustomPrintables.cs
@@ -5,8 +5,19 @@ namespace playlistimport;
 
 public class CustomPrintables
 {
-    public static void PrintSongList(List<Song> list)
+    public static void PrintSongList(List<Song>? list)
     {
+        if (list == null)
+        {
+            Console.Write("List was null");
+            return;
+        }
+
+        if (list.Count == 0)
+        {
+            Console.Write("No songs matched your query!");
+            return;
+        }
         Table table = new Table(TableConfig.Unicode());
         table.From(list.ToArray());
         Console.Write(table.ToString());
